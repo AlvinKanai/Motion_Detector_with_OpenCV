6d223627555a0b9f11170399eb1b86a17aebe413
AlvinKanai <49444119+AlvinKanai@users.noreply.github.com>
2020-05-21 22:30:25 +0300

    bug fixes ... hover effect values

--- motion_graph.py
+++ motion_graph.py
@@ -1,15 +1,15 @@
 from motion import df
 from bokeh.plotting import figure, output_file, show
-from bokeh.model import HoverTool, ColumnDataSource
+from bokeh.models import HoverTool, ColumnDataSource
+
+df["Start_time"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_time"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
 cd=ColumnDataSource(df)
 
-df["Start_time"]=df["Start"].dt.strftime("%Y-%m%d %H:%M:%S")
-df["End_time"]=df["End"].dt.strftime("%Y-%m%d %H:%M:%S")
+plt=figure(x_axis_type="datetime",width=800,height=300,title="Motion graph")
 
-plt=figure(width=800,height=300,title="Motion graph")
-
-plt.quad(left="Start",right="End",bottom=0,top=2,color="purple" source=cd)
+p=plt.quad(left="Start",right="End",bottom=0,top=2,color="purple", source=cd)
 
 plt.ygrid[0].ticker.desired_num_ticks=1
 plt.yaxis.minor_tick_line_color=None
@@ -17,5 +17,5 @@ plt.yaxis.minor_tick_line_color=None
 hover=HoverTool(tooltips=[("Entered","@Start_time"),("Left","@End_time")])
 plt.add_tools(hover)
 
-output_file("Motion.html")
+output_file("Motion.html",mode='absolute')
 show(plt)
